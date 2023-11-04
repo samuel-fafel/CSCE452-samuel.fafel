b5e89f23a9634909d3216f9284f2a853fa507659
ndcoles1420 <79115258+ndcoles1420@users.noreply.github.com>
2023-11-04 17:55:59 -0500

    Update launch.py (specs met)
    Note: For bag record to work, you must pass a new bag file into the argument. It cannot overwrite existing bag files. This will lead to an error and bag record will not happen.

--- project3_ws/install/project3/share/project3/launch.py
+++ project3_ws/install/project3/share/project3/launch.py
@@ -2,6 +2,8 @@ from launch import LaunchDescription
 from launch_ros.actions import *
 from launch.substitutions import *
 from launch.actions import *
+from launch.event_handlers import *
+from launch.events import *
 
 def generate_launch_description():
 
@@ -10,7 +12,22 @@ def generate_launch_description():
     #Must put full path in bag_in arg
 
     bag_in = DeclareLaunchArgument('bag_in')
-    #bag_out = DeclareLaunchArgument('bag_out')
+    bag_out = DeclareLaunchArgument('bag_out')
+
+    bag_play = ExecuteProcess(
+            cmd=['ros2', 'bag', 'play', LaunchConfiguration('bag_in')],
+            output='screen',
+            shell=True
+        )
+    
+    bag_record = ExecuteProcess(
+            #Put desired topics after LaunchConfiguration
+            cmd=['ros2', 'bag', 'record', '-o', LaunchConfiguration('bag_out'), '/scan', '/person_locations', '/person_count'],
+            output='screen',
+            shell=True
+        ) 
+
+    event_handler = OnProcessExit(target_action = bag_play, on_exit = [EmitEvent(event=Shutdown())])
 
     return LaunchDescription([
         Node(
@@ -26,15 +43,8 @@ def generate_launch_description():
             output='screen'
         ),
         bag_in,
-        #bag_out,
-        ExecuteProcess(
-            cmd=['ros2', 'bag', 'play', LaunchConfiguration('bag_in')],
-            output='screen',
-            shell=True
-        ),
-        #ExecuteProcess(
-            #cmd=['ros2', 'bag', 'record', LaunchConfiguration('bag_out')],
-            #output='screen',
-            #shell=True
-        #)
+        bag_out,
+        bag_play,
+        bag_record,
+        RegisterEventHandler(event_handler)
     ])
