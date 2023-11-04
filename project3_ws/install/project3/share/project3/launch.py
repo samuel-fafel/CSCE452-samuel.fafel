052e6abfebdc6adf4d37e2e911808b939014ca80
ndcoles1420 <79115258+ndcoles1420@users.noreply.github.com>
2023-11-04 16:10:08 -0500

    Update launch.py

--- project3_ws/install/project3/share/project3/launch.py
+++ project3_ws/install/project3/share/project3/launch.py
@@ -1,7 +1,17 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import *
+from launch.substitutions import *
+from launch.actions import *
 
 def generate_launch_description():
+
+    #Commented out code is for recording results
+    #Not needed until that part is done
+    #Must put full path in bag_in arg
+
+    bag_in = DeclareLaunchArgument('bag_in')
+    #bag_out = DeclareLaunchArgument('bag_out')
+
     return LaunchDescription([
         Node(
             package='project3',
@@ -15,4 +25,16 @@ def generate_launch_description():
             name='people_counter',
             output='screen'
         ),
+        bag_in,
+        #bag_out,
+        ExecuteProcess(
+            cmd=['ros2', 'bag', 'play', LaunchConfiguration('bag_in')],
+            output='screen',
+            shell=True
+        ),
+        #ExecuteProcess(
+            #cmd=['ros2', 'bag', 'record', LaunchConfiguration('bag_out')],
+            #output='screen',
+            #shell=True
+        #)
     ])
