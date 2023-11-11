9072619c9f6e123014a3f0599c2cfd7136983e55
samuel-fafel <samuel.fafel@gmail.com>
2023-11-11 17:34:58 -0600

    Removed excess

--- build/project2_tam/build/lib/project2_tam/background.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def main(args=None):
-	os.system("ros2 param set /turtlesim background_r 80")
-	os.system("ros2 param set /turtlesim background_g 0")
-	os.system("ros2 param set /turtlesim background_b 0")
-
-if __name__ == "__main__":
-    main()
--- build/project2_tam/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/samuel/ros2_humble/CSCE452-samuel.fafel/install/project2_tam'
